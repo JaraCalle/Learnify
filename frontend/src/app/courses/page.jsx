07357cae1d36b276b5e006735df37b768b1e0d31
Max <mbustamang@eafit.edu.co>
2025-05-13 18:21:38 -0500

    Infinite scroll funcionality for courses page

--- frontend/src/app/courses/page.jsx
+++ frontend/src/app/courses/page.jsx
@@ -1,36 +1,72 @@
+"use client";
+
 import React from "react";
 import CourseCard from "@components/Courses/CourseCard";
-import axios from "axios";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchCourses } from "@services/courseService";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { Spinner } from "@components/ui/Spinner";
 
-async function Courses({searchParams}) {
+function Courses({ searchParams }) {
+  const { search } = React.use(searchParams)  ;
+  const { ref, inView } = useInView();
 
-const { search } = await searchParams;
-
-  const url = new URL("http://127.0.0.1:8000/courses/");
-
-  if (search) {
-    url.searchParams.append("search", search);
-  }
-
-  const courses = await axios.get(url.toString())
-  .then((res) => {
-    const data = res.data;
-    const courses = data.results;
-
-    return courses
-  })
-  .catch((error) => {
-    console.error("Error fetching courses", error);
-    return [];
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ["courses", search],
+    queryFn: ({ pageParam = 1 }) => fetchCourses({ page: pageParam, search }),
+    getNextPageParam: (lastPage) => {
+      if (lastPage.next) {
+        const url = new URL(lastPage.next);
+        return url.searchParams.get("page");
+      }
+      return undefined;
+    },
   });
 
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
 
+  if (isLoading) {
+    return (
+      <div className="flex flex-col w-full items-center justify-start page-wrapper">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex flex-col w-full items-center justify-start page-wrapper">
+        <p>Error loading courses: {error.message}</p>
+      </div>
+    );
+  }
+
+  const courses = data?.pages.flatMap((page) => page.results) ?? [];
 
   return (
     <div className="flex flex-col w-full items-center justify-start page-wrapper">
       {courses.map((course, index) => (
-        <CourseCard key={index} course={course} />
+        <CourseCard key={course.id} course={course} />
       ))}
+
+      {/* Loading indicator and intersection observer target */}
+      <div ref={ref} className="w-full flex justify-center py-4">
+        {isFetchingNextPage && <Spinner />}
+      </div>
+
+      {!hasNextPage && <p>No more courses to load</p>}
     </div>
   );
 }
