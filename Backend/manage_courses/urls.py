8fe0efd9f7166d5f8d9b8e62efe6b28b989eb28d
val <vvhornung@eafit.edu.co>
2025-05-13 22:38:08 -0500

    Added owned courses functionality

--- Backend/manage_courses/urls.py
+++ Backend/manage_courses/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CourseListCreateView, CourseDetailView
+from .views import CourseListCreateView, CourseDetailView, get_owned_courses
 
 urlpatterns = [
     path('', CourseListCreateView.as_view(), name='course_list_create'),
     path('<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
-    
+    path('owned/', get_owned_courses, name='owned_courses'),
 ]
