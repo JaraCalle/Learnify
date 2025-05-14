7342b614c9b1ac56bd215bc847ed0bbef0d58df5
Juan Jara <jaracalle2041@gmail.com>
2025-05-14 01:46:27 -0500

    API de compañeros

--- Backend/manage_courses/views.py
+++ Backend/manage_courses/views.py
@@ -1,3 +1,5 @@
+import requests
+import os
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.filters import SearchFilter
@@ -5,6 +7,33 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Course
 from manage_courses.serializers import CourseSerializer
+from manage_courses.reports.pdf_report import PDFReportGenerator
+from manage_courses.reports.excel_report import ExcelReportGenerator
+from manage_courses.reports.interfaces import ReportGenerator
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def export_courses_report(request):
+    format = request.query_params.get('format', 'pdf')
+    courses = Course.objects.all()
+    # Inversión de dependencia
+    generator: ReportGenerator
+    if format == 'excel':
+        generator = ExcelReportGenerator()
+    else:
+        generator = PDFReportGenerator()
+
+    return generator.generate(courses)
+
+@api_view(['GET'])
+def fetch_external_nfts(request):
+    external_api_url = os.getenv('EXTERNAL_API_BASE_URL')
+    try:
+        response = requests.get(external_api_url, timeout=5)
+        response.raise_for_status()
+        return Response(response.json())
+    except requests.RequestException as e:
+        return Response({'error': str(e)}, status=500)
 
 class CourseListCreateView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
